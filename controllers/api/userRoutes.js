b17b9c7bcf0c546d2132e0911dd038127d60158c
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-08-01 11:07:33 -0400

    Modified code: rearranged script links and main tags in hbs files so pages rendered properly and functionality works as expected separated login.js and create-account.js as it seemed they were interfering with each other in the same .js file login, create account, logout, and navigation when logged in vs not work properly.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,7 +24,7 @@ router.post('/login', async (req, res) => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
 
-            res.json({user: userData, message: 'You are now logged in.', loggedIn: req.session.logged_in});
+            res.status(200).json({user: userData, message: 'You are now logged in.', loggedIn: req.session.logged_in});
         });
         // res.redirect('/dashboard');
     } catch (err) {
@@ -33,7 +33,24 @@ router.post('/login', async (req, res) => {
 });
 
 //create account (POST) route
-
+router.post('/createaccount', async (req, res) => {
+    try {
+        //check to see if user exists in DB
+        const userData = await User.findOne({ where: {username: req.body.username} });
+        //if found, then alert user an account already exists for that usernmae; otherwise create user
+        if (userData) {
+            res.status(400).json({message: 'Username already exists.  Please try logging in or select a new username.'})
+            return;
+        }
+        const newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        })
+        res.status(200).json({message: 'User account created successfully. Please log in.'});
+    } catch (err) {
+      res.status(400).json({error: err})
+    };
+});
 
 //logout route
 router.post('/logout', (req, res) => {
--- public/js/create-acct.js
+++ public/js/create-acct.js
@@ -1,38 +1,34 @@
-//login user
-const loginFormHandler = async (event) => {
+//create account for user
+const createAccountFormHandler = async (event) => {
     event.preventDefault();
     event.stopPropagation();
 
     const username = document.querySelector('#user').value.trim();
     const password = document.querySelector('#pwd').value.trim();
 
-    console.log(username);
-    console.log(password);
-
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users/createaccount', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
         }); 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/login');
             response.json().then(data => {
                 alert(data.message);
             }) 
         } else {
-            document.location.replace('/login');
+            document.location.reload('/createaccount');
             response.json().then(data => {
                 alert(data.message);
-            })
-        };
+            });
+        } 
     } else {
-        document.location.reload('/login');
-        alert('Username and/or password invalid.  Please try again.');
-        };
-;}
+        document.location.reload('/createaccount');
+        alert('Please provide a valid username and password.')   
+    };
+};
 
 document
     .querySelector("#login-form")
-    .addEventListener("submit", loginFormHandler);
-
+    .addEventListener("submit", createAccountFormHandler);
